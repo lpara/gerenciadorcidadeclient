1a5e49598b9649716e0903c20303fa08b7c2c37f
lpara <lpara@DESKTOP-7L7H53H>
2019-03-02 10:12:11 -0300

    Iniciando ao repository do client

---
+++ gerenciador-cidades-client/src/main/java/com/desafio/gerenciador/cidade/client/GerenciadorCidadesClientApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.gerenciador.cidade.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCidadesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCidadesClientApplication.class, args);
+	}
+
+}
---
+++ gerenciador-cidades-client/src/test/java/com/desafio/gerenciador/cidade/client/GerenciadorCidadesClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.desafio.gerenciador.cidade.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciadorCidadesClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
