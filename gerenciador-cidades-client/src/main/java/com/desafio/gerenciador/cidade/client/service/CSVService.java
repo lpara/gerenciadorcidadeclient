c317c8b6813a04fc6eee83dc2a2fd46ddaf324ae
lpara <lpara@DESKTOP-7L7H53H>
2019-03-02 22:32:50 -0300

    Preparando para fazer a importação do arquivo CSV.

---
+++ gerenciador-cidades-client/src/main/java/com/desafio/gerenciador/cidade/client/service/CSVService.java
@@ -0,0 +1,6 @@
+package com.desafio.gerenciador.cidade.client.service;
+
+public class CSVService {
+
+	//TODO criar o processamento do arquivo CSV e as funcionalidades relacionadas.
+}
---
+++ gerenciador-cidades-client/src/main/java/com/desafio/gerenciador/cidade/client/web/ImportadorBean.java
@@ -0,0 +1,20 @@
+package com.desafio.gerenciador.cidade.client.web;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Bean importador do arquivo CSV
+ * @author lpara
+ *
+ */
+@Component
+public class ImportadorBean {
+
+//TODO o próximo passo será criar um método aqui, onde o intuito dele será o de validar o arquivo carregado e se for de fato CSV 
+//	armazenar em um atributo do bean "FILE".
+//TODO 2 criar um atributo para só mostrar a listagens dos itens na tela de load quando a pessoa de fato clicar em analisar arquivo.
+// na listagem dos itens terá outro botão que confirma o envio do arquivo 
+//TODO 3 por fim terei de criar as opções de funcionalidades nessa página para poder acatar as exigências do desafio.
+	
+	
+}
